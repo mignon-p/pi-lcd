e96aacf303016b74ac1df7974cb9e1f45fdeadc9
Patrick Pelletier <code@funwithsoftware.org>
2017-01-31 03:46:29 +0000

    print buttons by name

--- Main.hs
+++ Main.hs
@@ -6,21 +6,21 @@ import Text.Printf
 import I2C
 import PiLcd
 
-printChanges :: PiLcd -> Int -> Word8 -> Int -> IO ()
-printChanges lcd addr old color = do
-  b <- getButtons lcd
-  color' <- if b == old
-            then return color
-            else do
-              let nc = 7 .&. (color + 1)
-                  nc' = toEnum nc
-              setBacklightColor lcd nc'
-              putStrLn $ printf "%02x %s" b (show nc')
-              return nc
-  printChanges lcd addr b color'
+printChanges :: PiLcd -> Int -> Int -> IO ()
+printChanges lcd addr color = do
+  b <- getButtonEvent lcd
+  color' <- case b of
+              Nothing -> return color
+              (Just btn) -> do
+                let nc = 7 .&. (color + 1)
+                    nc' = toEnum nc
+                setBacklightColor lcd nc'
+                putStrLn $ (show btn) ++ " " ++ (show nc')
+                return nc
+  printChanges lcd addr color'
 
 main = do
   h <- i2cOpen 1
   lcd <- mkPiLcd h
-  printChanges lcd 0x20 0xff 0
+  printChanges lcd 0x20 0
   i2cClose h
